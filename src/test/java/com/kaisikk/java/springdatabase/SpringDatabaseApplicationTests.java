81ca6bd3512482033d760006ab027b9633c51b4e
Kaisikk <Kosikov.Ilya@mail.ru>
2024-03-31 23:15:11 +0400

    Create Project

---
+++ src/test/java/com/kaisikk/java/springdatabase/SpringDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaisikk.java.springdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
