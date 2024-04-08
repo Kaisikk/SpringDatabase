2514d16792e6cea1a70314c6b946ee04b13fb1b3
Kaisikk <Kosikov.Ilya@mail.ru>
2024-04-08 22:25:29 +0400

    Работа с вебсокетами Проверял количество потоков вызываемых при работе с веб сокетами

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,11 +1,11 @@
 ws = new WebSocket("ws://localhost:8080/pong");
 
 ws.onopen = function () {
-    action('open connection');
+    // action('open connection');
 }
 
 ws.onmessage = function (ev) {
-    action(ev.data);
+    actionSec(ev.data);
 }
 
 ws.onerror = function (ev) {
@@ -15,15 +15,29 @@ ws.onclose = function (ev) {
 }
 
 
-function action(message) {
-    var output = document.getElementById("stack");
+// function action(message) {
+//     var output = document.getElementById("stack");
+//     var newP = document.createElement('p');
+//     newP.appendChild(document.createTextNode(message));
+//     output.appendChild(newP);
+// }
+//
+// function ping() {
+//     var message = document.getElementById('message').value;
+//     action('sent: ' + message);
+//     ws.send(message);
+// }
+
+function actionSec(message){
+    var output = document.getElementById("inscroll");
     var newP = document.createElement('p');
     newP.appendChild(document.createTextNode(message));
     output.appendChild(newP);
 }
 
-function ping() {
-    var message = document.getElementById('message').value;
-    action('sent: ' + message);
-    ws.send(message);
+window.onmousemove = function(e){
+    var xx = $(document).height()*.9-e.y
+    if(xx<0){
+        ws.send("load");
+    }
 }
